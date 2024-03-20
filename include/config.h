f29fd702fd811ba3dc6bc458b6c5fb031c2c6148
farkas93 <zsombor.kalotay@gmx.ch>
2024-03-20 20:18:27 +0100

    remove config.h, since not intended to have it here publicly...

--- include/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-#define WIFI_SSID "FarkasNET"
-#define WIFI_PASSWORD "dmaK7ZsMF$13iySnn&q"
-#define MQTT_SERVER "192.168.1.122"
-#define MQTT_TOPIC "robotControl"
-#endif
-
